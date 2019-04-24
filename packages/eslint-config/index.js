e99a258e18e59ed0d3ede77df3a30a5346eb823c
Victor <victor@foursquare.com>
2019-04-24 17:45:48 -0400

    chore(@foursquare/eslint-config): initial commit (#2)

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,7 @@
+// Copyright 2019 Foursquare Labs Inc. All Rights Reserved.
+
+/**
+ * See here for more information about sharing eslint config:
+ * https://eslint.org/docs/developer-guide/shareable-configs#creating-a-shareable-config
+ */
+module.exports = require('./eslintrc.json');
