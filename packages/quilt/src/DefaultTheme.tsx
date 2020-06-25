9300f8522185d11845a40c7b2fb40e9e9c74309f
Steven Lam <stevo14850@gmail.com>
2020-06-25 17:51:30 -0400

    remove roboto font loading in default theme

--- packages/quilt/src/DefaultTheme.tsx
+++ packages/quilt/src/DefaultTheme.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import { Provider } from './context';
 import { theme as defaultTheme } from './themes/default/theme';
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap');
-`;
 
 class DefaultTheme extends React.Component {
   render() {
     return (
       <>
-        <GlobalStyle />
         <Provider value={defaultTheme}>{this.props.children}</Provider>
       </>
     );
